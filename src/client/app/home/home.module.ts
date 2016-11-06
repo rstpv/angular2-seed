6eec305eb9b72704d59bc6412efa23beea3ca390
Roger - <stivenp20@gmail.com>
2016-11-06 13:17:53 -0500

    Correction to aot compile: Every directive must be associated to a module according to https://github.com/angular/angular/commit/7221632

--- src/client/app/home/home.module.ts
+++ src/client/app/home/home.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home.component';
 import { NameListService } from '../shared/name-list/index';
-import { PaperInputDirective,PaperCheckboxDirective } from '../shared/polymer/polymerdirective';
+import { PaperInputDirective,PaperCheckboxDirective,PaperRadioGroupDirective } from '../shared/polymer/polymerdirective';
 
 
 
 @NgModule({
   imports: [CommonModule, SharedModule],
-  declarations: [HomeComponent,PaperInputDirective,PaperCheckboxDirective],
+  declarations: [HomeComponent,PaperInputDirective,PaperCheckboxDirective,PaperRadioGroupDirective],
   exports: [HomeComponent],
   providers: [NameListService],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
